942a80d44c6529da77a243e072133473ca9a635c
Alexander Murphy <alex.murphy@ibotta.com>
2019-12-15 16:29:31 -0700

    Initial Commit

---
+++ tree_viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  tree_viewer
+//
+//  Created by Alex Murphy on 12/15/19.
+//  Copyright © 2019 Ibotta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
